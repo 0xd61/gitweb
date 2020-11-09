ededa65bc2d42db2b137fe43852730b3c6bc2514
kaitsh <kaitsh@degit.co>
2020-11-09 18:12:11 -0300

    Add basic auth for http

--- service.sh
+++ service.sh
@@ -6,6 +6,9 @@ GITWEB_CONF=/etc/gitweb.conf
 SERVER_DIR=/git
 SYNC_SCRIPT=/sync.sh
 
+# TODO(dgl): to load config on each loop
+# we have to check for changes and update the
+# authorized_keys file.
 . /.env
 
 # ---- Setup git ----
@@ -16,19 +19,27 @@ su $USER -c sh << EOC
 
 #[ -f ~/.ssh/id_rsa ] || ssh-keygen -f ~/.ssh/id_rsa -b 2048 -t rsa -q -N ""
 
-[ -d ~/.ssh ] || mkdir -p ~/.ssh
-echo "${PRIVATE_KEY}" > ~/.ssh/id_rsa
-echo "${AUTHORIZED_KEYS}" > ~/.ssh/authorized_keys
+[ -d ~/.ssh ] || mkdir -p /home/$USER/.ssh
+echo "${PRIVATE_KEY}" > /home/$USER/.ssh/id_rsa
+echo "${AUTHORIZED_KEYS}" > /home/$USER/.ssh/authorized_keys
+echo "${BASIC_AUTH}" > /home/$USER/.htpasswd
 cat << EOF > ~/.ssh/config
 Host *
   IdentityFile ~/.ssh/id_rsa
   StrictHostKeyChecking no
 EOF
 
-chmod 600 ~/.ssh/id_rsa
-chmod 600 ~/.ssh/authorized_keys
+
+
+chmod 600 /home/$USER/.ssh/id_rsa
+chmod 600 /home/$USER/.ssh/authorized_keys
+chmod 600 /home/$USER/.htpasswd
 
 EOC
+
+# TODO(dgl): We get a file permission error while cloning via http. I don't know why it uses the root config
+# TODO(dgl): chown did not work...
+#chown -R git:www-data /root/.config/git
 
 #cat $APACHE_CONFIG_DIR/gitServer.xml >> $APACHE_CONFIG_DIR/conf/httpd.conf
 #sed -i '/LoadModule alias_module modules\/mod_alias.so/aLoadModule cgi_module modules/mod_cgi.so' $APACHE_CONFIG_DIR/conf/httpd.conf
@@ -41,25 +52,42 @@ mv $NGINX_CONF $NGINX_CONF.original
 
 cat << EOF > $NGINX_CONF
 #https://stackoverflow.com/questions/6414227/how-to-serve-git-through-http-via-nginx-with-user-password
+#server {
+#    listen 80;
+#    server_name _;
+#    return 301 https://$host$request_uri;
+#}
+
 server {
-    listen       80; # Replace 443 ssl by 80 if you don't want TLS
+    listen       80;#443 ssl; # Replace 443 ssl by 80 if you don't want TLS
     server_name  _;
     root         /usr/share/gitweb; # Remove if you don't want Gitweb
 
-    error_log  /home/git/nginx-error.log;
-    access_log /home/git/nginx-access.log;
+    error_log  /var/log/nginx/error.log;
+    access_log /var/log/nginx/access.log;
+
+    # Remove ssl_* lines if you don't want TLS
+    #ssl_certificate /etc/letsencrypt/live/git.example.com/fullchain.pem;
+    #ssl_certificate_key /etc/letsencrypt/live/git.example.com/privkey.pem;
+    #ssl_protocols             TLSv1 TLSv1.1 TLSv1.2 TLSv1.3;
+    #ssl_prefer_server_ciphers on;
+    #ssl_ciphers               'EECDH+AESGCM:EDH+AESGCM:AES256+EECDH:AES256+EDH';
 
     # Remove auth_* if you don't want HTTP Basic Auth
-    #auth_basic "example Git";
-    #auth_basic_user_file /etc/nginx/.htpasswd;
 
     # static repo files for cloning over http
     location ~ ^.*\.git/objects/([0-9a-f]+/[0-9a-f]+|pack/pack-[0-9a-f]+.(pack|idx))$ {
+        limit_except GET { auth_basic off; }
+        auth_basic "Resticted Git";
+        auth_basic_user_file /home/$USER/.htpasswd;
         root $SERVER_DIR;
     }
 
     # requests that need to go to git-http-backend
     location ~ ^.*\.git/(HEAD|info/refs|objects/info/.*|git-(upload|receive)-pack)$ {
+        limit_except GET { auth_basic off; }
+        auth_basic "Resticted Git";
+        auth_basic_user_file /home/$USER/.htpasswd;
         root $SERVER_DIR;
 
         fastcgi_pass  unix:/var/run/fcgiwrap/fcgiwrap.sock;
@@ -74,6 +102,9 @@ server {
     # Remove all conf beyond if you don't want Gitweb
     try_files \$uri @gitweb;
     location @gitweb {
+        limit_except GET { auth_basic off; }
+        auth_basic "Resticted Git";
+        auth_basic_user_file /home/$USER/.htpasswd;
         fastcgi_pass  unix:/var/run/fcgiwrap/fcgiwrap.sock;
         fastcgi_param SCRIPT_FILENAME   /usr/share/gitweb/gitweb.cgi;
         fastcgi_param PATH_INFO         \$uri;
@@ -83,8 +114,8 @@ server {
 }
 EOF
 
-sed -i 's/user  nginx;/user fcgiwrap www-data;/' /etc/nginx/nginx.conf
-chown -R fcgiwrap:www-data /usr/share/gitweb
+sed -i 's/user  nginx;/user git www-data;/' /etc/nginx/nginx.conf
+chown -R git:www-data /usr/share/gitweb
 
 # Start sshd
 ssh-keygen -A
@@ -96,7 +127,7 @@ if [ $status -ne 0 ]; then
 fi
 
 # Start fcgiwrap
-/usr/bin/spawn-fcgi -s /var/run/fcgiwrap/fcgiwrap.sock -u fcgiwrap -g www-data /usr/bin/fcgiwrap
+/usr/bin/spawn-fcgi -s /var/run/fcgiwrap/fcgiwrap.sock -u $USER -g www-data /usr/bin/fcgiwrap
 status=$?
 if [ $status -ne 0 ]; then
   echo "Failed to start fcgiwrap: $status"
